c8711b36daac4d7b81a0a2ad4897cf15bf1c6f04
DESKTOP-2CGHMPC\WLZX <leeskiikseel@outlook.com>
2024-03-21 21:50:49 +0800

    add radix ui

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,5 +1,20 @@
+"use client";
 import React from "react";
+import {
+  Button,
+  TextFieldRoot,
+  TextFieldInput,
+  TextArea,
+} from "@radix-ui/themes";
 
 export default function Page() {
-  return <div>Issues</div>;
+  return (
+    <div className="flex flex-col gap-3 w-1/2">
+      <TextFieldRoot>
+        <TextFieldInput placeholder="Title" />
+      </TextFieldRoot>
+      <TextArea placeholder="Description" />
+      <Button className="w-48">Submit New Issue</Button>
+    </div>
+  );
 }
