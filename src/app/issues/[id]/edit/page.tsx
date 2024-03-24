b1f2647f8c28a81f66a844ce0855b21a643a5c78
DESKTOP-2CGHMPC\WLZX <leeskiikseel@outlook.com>
2024-03-24 16:55:59 +0800

    fix bug

--- src/app/issues/[id]/edit/page.tsx
+++ src/app/issues/[id]/edit/page.tsx
@@ -1,10 +1,14 @@
 import prisma from "@/lib/prisma";
 
 import IssueForm from "@/app/issues/_components/IssueForm";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const issue = await prisma.issue.findUnique({
     where: { id: Number(params.id) },
   });
+  if (!issue) {
+    notFound();
+  }
   return <IssueForm issue={issue} />;
 }
