3cdd63778d4f0cd017c3e0f573d9f4979e4e52cb
DESKTOP-TJ325BT\Administrator <leeskiikseel@outlook.com>
2024-03-26 14:21:03 +0800

    add credentials based account and password

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -12,6 +13,30 @@ const config = {
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    CredentialsProvider({
+      credentials: {
+        username: { label: "Username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+        const authResponse = await fetch("/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
+        });
+
+        if (!authResponse.ok) {
+          return null;
+        }
+
+        const user = await authResponse.json();
+
+        return user;
+      },
     }),
   ],
   // adapter: PrismaAdapter(prisma),
